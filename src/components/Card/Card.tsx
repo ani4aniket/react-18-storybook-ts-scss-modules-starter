602d03e30754136cec9fffb840bbb91089135aea
Aniket Kumar <aniketkumar049@gmail.com>
2023-02-16 19:30:42 +0530

    Add support for scss modules

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,9 @@
-import React, {type FC} from 'react'
-import { CardPropTypes } from './CardPropTypes'
-import styles from './Card.module.css'
+import React, { FC } from "react";
+import { CardPropTypes } from "./CardPropTypes";
+import styles from "./index.module.scss";
 
-const Card: FC<CardPropTypes> = (props) =>{
-    return <div className={`${styles.card}`}>{props.text}</div>
-}
+const Card: FC<CardPropTypes> = (props) => {
+  return <div className={styles.card}>{props.text}</div>;
+};
 
-export default Card
+export default Card;
